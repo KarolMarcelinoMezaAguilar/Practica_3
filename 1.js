3385a7bfa8195f1b9c2ebb96dddeb8cf69ae0917
Karol Marcelino Meza Aguilar <kmeza@ucol.mx>
2022-02-22 22:53:43 -0600

    string classic interpolation

--- 1.js
+++ 1.js
@@ -2,3 +2,9 @@ let logGreeting = (miParametro) => miParametro();
 logGreeting(function () {
     console.log('Hello from a function created on the fly');
 });
+
+//interpolación de strings "clásica"
+let logGreeting1 = (miNombre, miColor) => {
+    console-log("Hola " + miNombre + " buenos días! Tu color favorito es el " + miColor);
+}
+logGreeting1("Carlos", "Azul");
